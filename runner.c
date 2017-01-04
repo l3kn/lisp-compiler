c6ed9dbf1eed10b8d58e28dc9e40f0c6b6735b77
Leon Rische <leon.rische@student.uni-siegen.de>
2017-01-04 01:32:44 +0100

    Change to use 64bit assembly

--- runner.c
+++ runner.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
 #define wordsize 4
 
@@ -40,7 +43,66 @@ static void print_ptr(ptr x) {
   printf("\n");
 }
 
+static char* allocate_protected_space(int size) {
+  int page = getpagesize();
+  int status;
+
+  // Round to some size that is a multiple of page
+  // and bigger or equal to the original size
+  int aligned_size = ((size + page - 1) / page) * page;
+
+  // Allocate the aligned size
+  // and two additional pages
+  char* p = mmap(0, aligned_size + 2 * page,
+                 PROT_READ | PROT_WRITE,
+                 MAP_ANONYMOUS | MAP_PRIVATE,
+                 0, 0);
+  if (p == MAP_FAILED) {
+    printf("Failed to allocate");
+    exit(EXIT_FAILURE);
+  }
+
+  // Protect the first and the last page
+  // so the program crashes
+  // if the stack over- or underflows
+  status = mprotect(p, page, PROT_NONE);
+  if (status != 0) {
+    printf("Failed to protect the first page");
+    exit(EXIT_FAILURE);
+  }
+
+  status = mprotect(p + page + aligned_size, page, PROT_NONE);
+  if (status != 0) {
+    printf("Failed to protect the last page");
+    exit(EXIT_FAILURE);
+  }
+
+  // Add an offset of one page
+  // because the first page is protected
+  return (p + page);
+}
+
+static void deallocate_protected_space(char* p, int size) {
+  int page = getpagesize();
+  int status;
+  int aligned_size = ((size + page - 1) / page) * page;
+
+  status = munmap(p - page, aligned_size + 2 * page);
+
+  if (status != 0) {
+    printf("Failed to deallocate");
+    exit(EXIT_FAILURE);
+  }
+}
+
 int main(int argc, char** argv) {
-  print_ptr(scheme_entry());
+  int stack_size = (16 * 4096); // 16k 32bit cells
+
+  char* stack_top = allocate_protected_space(stack_size);
+  char* stack_base = stack_top + stack_size;
+
+  print_ptr(scheme_entry(stack_base));
+
+  deallocate_protected_space(stack_top, stack_size);
   return 0;
 }
